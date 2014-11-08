9f554ea0f06a3e62301d17837ff41d47c546d47b
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-11-08 11:37:54 -0500

    commenting

--- src/localization/Direction.java
+++ src/localization/Direction.java
@@ -8,7 +8,19 @@ package localization;
  * @author Scott Coooper
  */
 public enum Direction {
-	UP(0), LEFT(1), RIGHT(3), DOWN(2);	// 4 possible directions
+	// 4 possible directions
+	
+	/** UP relative to the start */
+	UP(0), 
+	
+	/** LEFT relative to the start */
+	LEFT(1), 
+	
+	/** RIGHT relative to the start */
+	RIGHT(3), 
+	
+	/** DOWN relative to the start */
+	DOWN(2);	
 	public final int v;					// # of left turns from up
 	private Direction(int i){ v = i; }	// Direction constructor
 	
