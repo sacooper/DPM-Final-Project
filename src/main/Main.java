2ecbdb9e6587287ad145556e9c832aa673ec39f1
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-12-01 11:21:17 -0500

    Added maps for final demonstration

--- src/main/Main.java
+++ src/main/Main.java
@@ -47,7 +47,8 @@ public class Main {
 	private static Display display;
 	private static OdometryCorrection odoCorrection;
 	private static Arm arm;
-	private static long start, time;		// TODO: INITIALIZE AND USE
+	private static long start, time; 
+	private static boolean started;
 	
 	public static final float	 
 		LEFT_WHEEL_D = 4.155f,
@@ -56,222 +57,20 @@ public class Main {
 		TILE_WIDTH = 30.48f;
 	
 	public static final int
-		NUM_TILES = 8;							// FIXME
+		NUM_TILES = 12;
 	
 	// List of maps for use in competition
 	// usage: maps[map_number][x][y]
 	private static final BitSet[] maps;
 
-	private static final int NUM_MAPS = 3;		// FIXME
-	private static int mapNumber = 0;			// FIXME
+	private static final int NUM_MAPS = 6;
+	private static int mapNumber = 0;
 	private static Waypoint dropoff = null;
 	
 	/***
 	 * Private constructor to prevent external instantiation
 	 */
 	private Main(){};
-	
-//	public static void arm_lock_test(String[] args){
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		pilot.setAcceleration(500);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		ARM.stop(true);
-//		Button.waitForAnyPress();
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//		pilot.travel(Main.TILE_WIDTH);
-//		pilot.travel(-Main.TILE_WIDTH);
-//	}
-//	
-//	public static void block_test(String[] args){
-//		// Instantiate a new DifferentialPilot to control movement
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		pilot.setAcceleration(500);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-//		odo = new OdometryPoseProvider(pilot);
-//		odo.setPose(new Pose(0, 0, -90));
-//		nav = new Navigator(pilot, odo);
-//		arm = new Arm(ARM, ArmState.RAISED);
-//		blockRescuer = new BlockRescuer(pilot, ULTRASONIC, arm);
-//		Button.waitForAnyPress();
-//		blockRescuer.rescueBlock();
-//		pilot.travel(Main.TILE_WIDTH);
-//		arm.drop();		
-//	}
-//	
-//	public static void localizer_test(String[] args){
-//		// Instantiate a new DifferentialPilot to control movement
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		pilot.setAcceleration(2000);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-//		odo = new OdometryPoseProvider(pilot);
-//		display = new Display(odo);
-//		display.start();
-//		
-//		localizer = new Localizer(pilot, ULTRASONIC, odo);
-//		arm = new Arm(ARM);
-//		blockRescuer = new BlockRescuer(pilot, ULTRASONIC, arm);
-//		Button.waitForAnyPress();
-//		
-//		localizer.localize();
-//		
-//		Button.waitForAnyPress();
-//		
-//	}
-//	
-//	
-//	public static void odo_test_2(String[] args){
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		
-//		pilot.setAcceleration(500);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-//		odo = new OdometryPoseProvider(pilot);
-//		
-//		odoCorrection = new OdometryCorrection(odo, COLORSENSOR_LEFT, COLORSENSOR_RIGHT);
-//		
-//		odoCorrection.start();
-//	
-//		display = new Display(odo);
-//		display.start();
-//		
-//		odo.setPose(new Pose(-15, -15, 0));
-//		
-//		nav = new Navigator(pilot, odo);
-//		Button.waitForAnyPress();
-//
-//		OdometryCorrection.enable();
-//		Delay.msDelay(1000);
-//		
-//		Path p = new Path();
-//		p.add(new Waypoint(Main.TILE_WIDTH*1.5, -Main.TILE_WIDTH/2f));
-//		p.add(new Waypoint(Main.TILE_WIDTH*1.5, Main.TILE_WIDTH*1.5));
-//		p.add(new Waypoint(-Main.TILE_WIDTH/2f, Main.TILE_WIDTH*1.5));
-//		p.add(new Waypoint(-Main.TILE_WIDTH/2f, -Main.TILE_WIDTH/2f));
-//		
-//		for (Waypoint w : p){
-//			nav.goTo(w);
-//			Button.waitForAnyPress();
-//		}
-//		
-//		
-//		
-//	}
-//
-//	public static void odo_correction_test(String[] args){
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		DifferentialPilot pilot2 = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		
-//		pilot.setAcceleration(500);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		pilot2.setRotateSpeed(90);
-//		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-//		odo = new OdometryPoseProvider(pilot);
-//		
-//		odoCorrection = new OdometryCorrection(odo, COLORSENSOR_LEFT, COLORSENSOR_RIGHT);
-//		
-//		odoCorrection.start();
-//	
-//		display = new Display(odo);
-//		display.start();
-//		
-//		Button.waitForAnyPress();
-//		
-//		pilot2.rotate(20);
-//
-//		OdometryCorrection.enable();
-//		Delay.msDelay(1000);
-//		pilot.travel(Main.TILE_WIDTH*1.5f);
-//		
-//		
-//		
-//	}
-//	
-//	public static void nav_test(String[] args){
-//		Button.waitForAnyPress();
-//		// Instantiate a new DifferentialPilot to control movement
-//		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-//		pilot.setAcceleration(2000);
-//		pilot.setTravelSpeed(22);
-//		pilot.setRotateSpeed(90);
-//		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-//		odo = new OdometryPoseProvider(pilot);
-//		
-//		// Instantate a new OdometryCorrection and disable it
-//		odoCorrection = new OdometryCorrection(odo, COLORSENSOR_LEFT, COLORSENSOR_RIGHT);
-//		odo.setPose(new Pose(-15, -15, 90));
-//		nav = new Navigator(pilot, odo);
-//		moveController = new MovementController(nav);
-//		odoCorrection.start();
-//		moveController.travelToWaypoint(new Waypoint(Main.TILE_WIDTH, 2.5*Main.TILE_WIDTH, 0));
-//		
-//	}
-	
-	
-	public static void _main(String[] args){
-		// Instantiate a new DifferentialPilot to control movement
-		pilot = new DifferentialPilot(LEFT_WHEEL_D, RIGHT_WHEEL_D, WHEEL_BASE, MOTOR_LEFT, MOTOR_RIGHT, false);
-		pilot.setAcceleration(500);
-		pilot.setTravelSpeed(22);
-		pilot.setRotateSpeed(90);
-		// Instantiate a new OdometryPoseProvider, of maintaining current pose
-		odo = new OdometryPoseProvider(pilot);
-		
-		// Instantate a new OdometryCorrection and disable it
-		odoCorrection = new OdometryCorrection(odo, COLORSENSOR_LEFT, COLORSENSOR_RIGHT);
-		
-		display = new Display(odo);
-		
-		// Instantiate a new Localizer
-		localizer = new Localizer(pilot, ULTRASONIC, odo);
-		
-		// Instantiate a new Navigator to control movement
-		nav = new Navigator(pilot, odo);
-		
-		// Instantiate a new MovementController for travelling to waypoints
-		moveController = new MovementController(nav);
-
-		// Instantiate a new Arm for controlling claw movement
-		arm = new Arm(ARM, Arm.ArmState.RAISED);
-		
-		// Instantiate a new blockRescuer
-		blockRescuer = new BlockRescuer(pilot, nav, ULTRASONIC, arm);
-		odoCorrection.start();
-		
-		
-		display.start();
-		
-		Button.waitForAnyPress();
-		
-//		odo.setPose(new Pose(Main.TILE_WIDTH*1.5f, 0, -90));
-//		blockRescuer.rescueBlock();
-		odo.setPose(new Pose(Main.TILE_WIDTH*-.5f, Main.TILE_WIDTH*-.5f, 90));
-		OdometryCorrection.enable();
-		moveController.travelToTile(3, 3);
-		moveController.travelToTile(3, 0);
-		moveController.travelToTile(0, 0);
-		moveController.travelToTile(3, 3);
-	}
-	
 	
 	public static void main(String[] args) {
 		setup();
@@ -286,7 +85,8 @@ public class Main {
 		}
 		
 		
-		Button.waitForAnyPress();
+		while (Button.waitForAnyPress() != Button.ID_ENTER);
+		started = true;
 		start = System.currentTimeMillis();
 		time = -1;
 		
@@ -391,12 +191,14 @@ public class Main {
 	}
 	
 	static {
+		started = false;
+		
 		// Add button listener to escape button to allow for exiting at any time
 		Button.ESCAPE.addButtonListener(
 				new ButtonListener(){
 					@Override
 					public void buttonPressed(Button b) {
-						if (b.getId() == Button.ID_ESCAPE) System.exit(0);}
+						if (started && b.getId() == Button.ID_ESCAPE) System.exit(0);}
 		
 					@Override
 					public void buttonReleased(Button b) {}});
@@ -409,14 +211,6 @@ public class Main {
 					maps[i] = new BitSet(Main.NUM_TILES * Main.NUM_TILES);
 					maps[i].clear();}}}
 	
-	/**********************************************
-		// The following is from Lab 5 :
-		maps[0].set(0*Main.NUM_TILES + 3, true);	// (0,3)
-		maps[0].set(1*Main.NUM_TILES + 0, true);	// (1,0)
-		maps[0].set(2*Main.NUM_TILES + 2, true);	// (2,2)
-		maps[0].set(3*Main.NUM_TILES + 2, true);	// (3,2)
-		//////////////////////////////
-	************************************************/
 	/**********************************************/
 		// The follow are for beta demonstrations
 		
@@ -473,13 +267,159 @@ public class Main {
 		setBlock(2, 7, 4, true);
 		setBlock(2, 7, 6, true);
 		
-		
-		// Initialization of map 3
-		
-		// Initialization of map 4
-		
-		// Initialization of map 5
 	/**********************************************/
+		
+	/**************************************************
+	 * The follow maps are for the FINAL DEMONSTRATION
+	 ************************************************/
+		
+		
+		// Map 1
+		setBlock(0, 0, 6, true);
+		setBlock(0, 0, 9, true);
+		setBlock(0, 1, 3, true);
+		setBlock(0, 2, 2, true);
+		setBlock(0, 2, 11, true);
+		setBlock(0, 3, 5, true);
+		setBlock(0, 3, 10, true);
+		setBlock(0, 4, 4, true);
+		setBlock(0, 4, 6, true);
+		setBlock(0, 5, 0, true);
+		setBlock(0, 5, 2, true);
+		setBlock(0, 6, 3, true);
+		setBlock(0, 7, 7, true);
+		setBlock(0, 8, 0, true);
+		setBlock(0, 8, 1, true);
+		setBlock(0, 8, 6, true);
+		setBlock(0, 9, 4, true);
+		setBlock(0, 9, 7, true);
+		setBlock(0, 9, 9, true);
+		setBlock(0, 10, 2, true);
+		setBlock(0, 10, 6, true);
+		setBlock(0, 11, 1, true);
+		
+		// Map 2
+		setBlock(1, 0, 4, true);
+		setBlock(1, 0, 5, true);
+		setBlock(1, 3, 2, true);
+		setBlock(1, 3, 5, true);
+		setBlock(1, 3, 6, true);
+		setBlock(1, 3, 9, true);
+		setBlock(1, 4, 5, true);
+		setBlock(1, 4, 9, true);
+		setBlock(1, 4, 10, true);
+		setBlock(1, 5, 5, true);
+		setBlock(1, 6, 8, true);
+		setBlock(1, 6, 9, true);
+		setBlock(1, 7, 0, true);
+		setBlock(1, 8, 4, true);
+		setBlock(1, 8, 5, true);
+		setBlock(1, 8, 10, true);
+		setBlock(1, 9, 1, true);
+		setBlock(1, 9, 11, true);
+		setBlock(1, 10, 7, true);
+		setBlock(1, 11, 2, true);
+		setBlock(1, 11, 5, true);
+		setBlock(1, 11, 7, true);
+
+		// Map 3
+		setBlock(2, 0, 3, true);
+		setBlock(2, 0, 8, true);
+		setBlock(2, 2, 3, true);
+		setBlock(2, 2, 6, true);
+		setBlock(2, 3, 2, true);
+		setBlock(2, 3, 4, true);
+		setBlock(2, 3, 10, true);
+		setBlock(2, 4, 1, true);
+		setBlock(2, 4, 5, true);
+		setBlock(2, 4, 6, true);
+		setBlock(2, 4, 7, true);
+		setBlock(2, 6, 6, true);
+		setBlock(2, 6, 8, true);
+		setBlock(2, 7, 0, true);
+		setBlock(2, 7, 11, true);
+		setBlock(2, 9, 6, true);
+		setBlock(2, 10, 0, true);
+		setBlock(2, 10, 3, true);
+		setBlock(2, 10, 4, true);
+		setBlock(2, 10, 9, true);
+		setBlock(2, 11, 2, true);
+		setBlock(2, 11, 11, true);
+
+		// Map 4
+		setBlock(3, 0, 2, true);
+		setBlock(3, 0, 3, true);
+		setBlock(3, 0, 4, true);
+		setBlock(3, 0, 8, true);
+		setBlock(3, 1, 4, true);
+		setBlock(3, 2, 0, true);
+		setBlock(3, 2, 5, true);
+		setBlock(3, 2, 10, true);
+		setBlock(3, 3, 2, true);
+		setBlock(3, 3, 9, true);
+		setBlock(3, 3, 11, true);
+		setBlock(3, 4, 3, true);
+		setBlock(3, 5, 10, true);
+		setBlock(3, 6, 4, true);
+		setBlock(3, 8, 4, true);
+		setBlock(3, 8, 8, true);
+		setBlock(3, 9, 0, true);
+		setBlock(3, 9, 4, true);
+		setBlock(3, 9, 6, true);
+		setBlock(3, 11, 1, true);
+		setBlock(3, 11, 5, true);
+		setBlock(3, 11, 10, true);
+		
+		// Map 5
+		setBlock(4, 0, 3, true);
+		setBlock(4, 0, 5, true);
+		setBlock(4, 0, 8, true);
+		setBlock(4, 3, 1, true);
+		setBlock(4, 3, 3, true);
+		setBlock(4, 3, 10, true);
+		setBlock(4, 4, 3, true);
+		setBlock(4, 4, 6, true);
+		setBlock(4, 4, 7, true);
+		setBlock(4, 4, 10, true);
+		setBlock(4, 5, 8, true);
+		setBlock(4, 6, 10, true);
+		setBlock(4, 7, 6, true);
+		setBlock(4, 7, 7, true);
+		setBlock(4, 8, 1, true);
+		setBlock(4, 8, 5, true);
+		setBlock(4, 8, 10, true);
+		setBlock(4, 9, 2, true);
+		setBlock(4, 9, 6, true);
+		setBlock(4, 10, 10, true);
+		setBlock(4, 11, 2, true);
+		setBlock(4, 11, 9, true);
+		
+		// Map 6
+		setBlock(5, 0, 10, true);
+		setBlock(5, 1, 5, true);
+		setBlock(5, 2, 3, true);
+		setBlock(5, 3, 2, true);
+		setBlock(5, 3, 10, true);
+		setBlock(5, 4, 2, true);
+		setBlock(5, 4, 7, true);
+		setBlock(5, 5, 2, true);
+		setBlock(5, 5, 4, true);
+		setBlock(5, 5, 7, true);
+		setBlock(5, 5, 11, true);
+		setBlock(5, 6, 5, true);
+		setBlock(5, 6, 9, true);
+		setBlock(5, 7, 6, true);
+		setBlock(5, 7, 8, true);
+		setBlock(5, 8, 2, true);
+		setBlock(5, 8, 10, true);
+		setBlock(5, 9, 0, true);
+		setBlock(5, 9, 4, true);
+		setBlock(5, 9, 8, true);
+		setBlock(5, 10, 5, true);
+		setBlock(5, 10, 10, true);
+		
+		
+		
 	}
 
 	private static void setBlock(int map, int x, int y, boolean v){
@@ -495,7 +435,7 @@ public class Main {
 		int x =0, y = 0, option;
 		do {
 			LCD.clear();
-			LCD.drawString("Map: " + mapNumber, 0, 0);
+			LCD.drawString("Map: " + (mapNumber + 1), 0, 0);
 			option = Button.waitForAnyPress();
 			switch (option){
 			case Button.ID_LEFT: mapNumber = (mapNumber + (Main.NUM_MAPS - 1)) % Main.NUM_MAPS; break;
